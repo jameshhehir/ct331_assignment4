27bb2ff42d9f5cf75530149c368abed0fccb7af6
James Hehir <j.hehir1@nuigalway.ie>
2017-11-13 18:45:20 +0000

    Attempt numbero 1

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,16 @@
+
 %Definitions for isElementInList(El, List)
+isElementInList(X,[X|_]).
+isElementInList(X,[_|T]):- isElementInList([X|T]).
 
 %Definitions for reverseList(List, ReversedList)
+reverseList([],[]).
+reverseList([H|T], ReversedList):-
+    reverseList(T, ReversedT), conc(ReversedT, [H], ReversedList).
 
 %insertElementIntoListEnd(El, List, NewList)
 
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists([], List2, List2).
+mergeLists([Item | Tail1], List2, [Item | Merged]) :-
+      mergeLists(Tail1, List2, Merged).
